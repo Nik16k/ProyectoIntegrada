e2d5bf5c7320d207a6badc45ee2130541a17ac26
danielduran <danielduran@skaphetecnologia>
2019-03-16 05:28:22 -0500

    Controller ->Historia

--- src/main/java/com/helloworld/apispring/controller/Controller.java
+++ src/main/java/com/helloworld/apispring/controller/Controller.java
@@ -1,6 +1,7 @@
 package com.helloworld.apispring.controller;
 
 import com.helloworld.apispring.model.entity.personas;
+import com.helloworld.apispring.model.entity.historia;
 import com.helloworld.apispring.model.entity.eventos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,28 +15,55 @@ import org.springframework.http.MediaType;
 public class Controller {
 
     @Autowired
-    private personaServicio personaServicio;
+    private personaServicio Ciudadano_Servicio;
+    @Autowired
+    private historiaServicio registroSituacionesServicio;
 
-// TRAER TODOS LOS CIUDADANOS DE LA BD
-    @RequestMapping(value = "/Personas", method = RequestMethod.GET)
+// Servicio No.1 Mostrar todos los personas creados en la database
+    @RequestMapping(value = "/Ciudadanos", method = RequestMethod.GET)
     public ResponseEntity<List<personas>> obtenerCiudadanos() {
-        List<personas> ciudadanos = personaServicio.getCiudadanos();
+        List<personas> ciudadanos = Ciudadano_Servicio.getCiudadanos();
         return new ResponseEntity<List<personas>>(ciudadanos, HttpStatus.OK);
     }
+//Servicio No. 2 Crear un ciudadano en la database
 
-    @RequestMapping(value = "/login/", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/ciudadanos", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> crearPersona(@RequestBody personas Ciudadano) {
-        String resultado = "Se creo persona con ID: " + personaServicio.crearPersona(Ciudadano);
+        String resultado = "Se creo persona con ID: " + Ciudadano_Servicio.crearPersona(Ciudadano);
+        return new ResponseEntity<String>(resultado, HttpStatus.OK);
+    }
+
+    @Autowired
+    private eventoServicio Situaciones_Servicio;
+
+//Servicio No 3 . Mostrar todas las eventos de los personas
+    @RequestMapping(value = "/eventos", method = RequestMethod.GET)
+    public ResponseEntity<List<eventos>> obtenerSituaciones() {
+        List<eventos> situaciones = Situaciones_Servicio.getSituaciones();
+        return new ResponseEntity<List<eventos>>(situaciones, HttpStatus.OK);
+    }
+    //Servicio No. 4 buscar ciudadano por id
+
+    @RequestMapping(value = "/personas", method = RequestMethod.GET)
+    public ResponseEntity<List<personas>> ciudadanoPorId(@RequestParam(value = "IdCiudadano") int id) {
+        List<personas> ciudadnosId = (List<personas>) Ciudadano_Servicio.getCiudadanoPorId(id);
+        return new ResponseEntity<List<personas>>(ciudadnosId, HttpStatus.OK);
+    }
+//Servicio No 5 registrar un evento por personas
+    @RequestMapping(value = "/eventos", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> crearEventoCiudadano(@RequestBody historia registroEvento) {
+        String resultado = "Se creo el evento : " + registroSituacionesServicio.registroEvento(registroEvento);
         return new ResponseEntity<String>(resultado, HttpStatus.OK);
     }
     
-    // VALIDAR EVENTOS POR CIUDADANO    
-    @Autowired
-    private eventoServicio eventosServicio;
-
-    @RequestMapping(value = "/Eventos_Ciudadano", method = RequestMethod.GET)
-    public ResponseEntity<List<eventos>> obtenerSituaciones() {
-        List<eventos> situaciones = eventosServicio.getSituaciones();
-        return new ResponseEntity<List<eventos>>(situaciones, HttpStatus.OK);
+    //Servicio No. 6 Listar Eventos por ciudadano
+    
+    @RequestMapping(value = "/eventos_persona", method = RequestMethod.GET)
+    public ResponseEntity<List<historia>> eventosCiudadanoPorId(@RequestParam(value = "IdCiudadano") int id) {
+        List<historia> eventosCiudadanosId = (List<historia>) registroSituacionesServicio.getEventosId(id); 
+                
+        return new ResponseEntity<List<historia>>(eventosCiudadanosId, HttpStatus.OK);
     }
+    
+    
 }
--- src/main/java/com/helloworld/apispring/controller/historiaServicio.java
+++ src/main/java/com/helloworld/apispring/controller/historiaServicio.java
@@ -1,23 +1,33 @@
 package com.helloworld.apispring.controller;
 
 import com.helloworld.apispring.model.dao.historiaRepositorio;
+import com.helloworld.apispring.model.dao.eventoRepositorio;
 import com.helloworld.apispring.model.entity.historia;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class RegistroSituaciones_Servicio {
+public class historiaServicio {
     
      @Autowired
     private historiaRepositorio RegistroSituaciones_Repositorio;
 
-    public RegistroSituaciones_Servicio() {
+    public historiaServicio() {
     }
 
     public List<historia> getRegistro() {
         return RegistroSituaciones_Repositorio.getRegistroSituaciones();
     }
+
+    public long registroEvento(historia registroEvento) {
+      return RegistroSituaciones_Repositorio.crearEvento(registroEvento);
+      
+    }
     
+      List<historia> getEventosId(int id) {
+        return (List<historia>) RegistroSituaciones_Repositorio.eventosIdCiudadano(id);
+    }
+   
     
 }
